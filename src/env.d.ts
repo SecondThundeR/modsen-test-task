30d23629bc551672ccb0c73b06a777b502891ba2
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-07 01:38:36 +0300

    feat: add new variables to .env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  readonly VITE_BOOKS_API_URL: string;
   readonly VITE_BOOKS_API_KEY: string;
+  readonly VITE_BOOKS_MAX_RESULTS: number;
 }
 
 interface ImportMeta {
