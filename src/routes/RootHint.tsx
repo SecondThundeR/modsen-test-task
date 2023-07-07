9da826ec5a7d76d1cac6015fe01e1064f6c343a2
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-08 00:39:02 +0300

    fix(routes): make root hint centered

--- src/routes/RootHint.tsx
+++ src/routes/RootHint.tsx
@@ -1,7 +1,7 @@
 export function RootHint() {
   return (
     <div className="flex justify-center items-center h-full">
-      <h1 className="text-xl font-bold opacity-60">Use the search above to get a list of books</h1>
+      <h1 className="text-xl font-bold opacity-60 text-center">Use the search above to get a list of books</h1>
     </div>
   );
 }
