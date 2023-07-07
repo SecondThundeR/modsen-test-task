b88bec783a14a9ee8b8768190188def3fd3ee8f4
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-08 00:22:26 +0300

    fix(components): update CardGrid
    Change cards parameters to pages (Reflect changes due to useInfinityQuery hook)
    Update items map checks

--- src/components/CardGrid/index.tsx
+++ src/components/CardGrid/index.tsx
@@ -3,21 +3,25 @@ import { BookCard } from "../BookCard";
 import { VolumesType } from "../../schemas/api/volumes";
 
 interface CardGridProps {
-  cards: VolumesType["items"];
+  pages: VolumesType[] | undefined;
 }
 
-export function CardGrid({ cards }: CardGridProps) {
+export function CardGrid({ pages }: CardGridProps) {
+  const itemsArray = pages?.flatMap(({ items }) => items);
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-      {cards?.map((card) => {
-        const volumeInfo = card.volumeInfo;
+      {itemsArray?.map((item) => {
+        if (!item) return null;
+
+        const volumeInfo = item?.volumeInfo;
         if (volumeInfo === undefined || volumeInfo === null) return null;
 
         const { imageLinks, title, categories, authors } = volumeInfo;
         return (
           <BookCard
-            key={card.id}
-            id={card.id}
+            key={item.id}
+            id={item.id}
             imageUrl={imageLinks?.smallThumbnail}
             title={title}
             categories={categories}
