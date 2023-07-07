b18cd089aec7c186f29a98a8beebf168381618bb
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-08 00:20:38 +0300

    fix(components): update BookCard
    Make Link be main container for card contents
    Make category use <p> tag to not conflict with parent <Link>

--- src/components/BookCard/index.tsx
+++ src/components/BookCard/index.tsx
@@ -21,14 +21,15 @@ export function BookCard({ id, imageUrl, categories, title, authors }: BookCardP
   const authorsInfo = authors?.join(", ") || "";
 
   return (
-    <Link to={`/books/${id}`}>
-      <div className="min-h-16 w-72 aspect-auto rounded-xl bg-base-200 flex flex-col gap-4 px-4 py-6 items-center duration-[350ms] ease-in-out hover:-translate-y-3 hover:drop-shadow-xl">
-        {imageUrl ? <img className="w-36 h-52 shadow-2xl" src={imageUrl} /> : <EmptyCover />}
-        <div className="flex flex-col items-start w-full gap-1">
-          <a className="link link-primary">{firstCategory}</a>
-          <h1 className="font-bold text-xl line-clamp-1">{title}</h1>
-          <p className="opacity-50">{authorsInfo}</p>
-        </div>
+    <Link
+      to={`/books/${id}`}
+      className="min-h-16 w-72 aspect-auto rounded-xl bg-base-200 flex flex-col gap-4 px-4 py-6 items-center duration-[350ms] ease-in-out hover:-translate-y-3 hover:drop-shadow-xl"
+    >
+      {imageUrl ? <img className="w-36 h-52 shadow-2xl" src={imageUrl} /> : <EmptyCover />}
+      <div className="flex flex-col items-start w-full gap-1">
+        <p className="link link-primary">{firstCategory}</p>
+        <h1 className="font-bold text-xl line-clamp-1">{title}</h1>
+        <p className="opacity-50">{authorsInfo}</p>
       </div>
     </Link>
   );
