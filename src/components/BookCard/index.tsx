2edce89bcc093bee6827ab8c139b8d3c12436deb
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-07 18:57:44 +0300

    fix(components): update BookCard
    Add shadow for empty cover
    Make authors accept null
    Show empty string when category/authors is not present
    Change check for imageUrl (In case there are empty string)

--- src/components/BookCard/index.tsx
+++ src/components/BookCard/index.tsx
@@ -2,24 +2,24 @@ interface BookCardProps {
   imageUrl: string | undefined;
   categories?: string[] | null;
   title: string;
-  authors: string[];
+  authors?: string[] | null;
 }
 
 function EmptyCover() {
   return (
-    <div className="w-36 h-52 bg-base-content flex items-center justify-center">
+    <div className="w-36 h-52 bg-base-content flex items-center justify-center shadow-2xl">
       <h1 className="text-base-300 text-xl">No cover</h1>
     </div>
   );
 }
 
 export function BookCard({ imageUrl, categories, title, authors }: BookCardProps) {
-  const firstCategory = categories?.[0] || "Unknown category";
-  const authorsInfo = authors.join(", ");
+  const firstCategory = categories?.[0] || "";
+  const authorsInfo = authors?.join(", ") || "";
 
   return (
     <div className="min-h-16 w-72 aspect-auto rounded-xl bg-base-200 flex flex-col gap-4 px-4 py-6 items-center">
-      {imageUrl !== undefined ? <img className="w-36 h-52 shadow-2xl" src={imageUrl} /> : <EmptyCover />}
+      {imageUrl ? <img className="w-36 h-52 shadow-2xl" src={imageUrl} /> : <EmptyCover />}
       <div className="flex flex-col items-start w-full gap-1">
         <a className="link link-primary">{firstCategory}</a>
         <h1 className="font-bold text-xl">{title}</h1>
