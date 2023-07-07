896bee457164b9a98b87a76a7c9a0f9a434f2c68
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-07 19:53:50 +0300

    feat(components): add Link component for home redirect

--- src/components/Header/HeaderTitle/index.tsx
+++ src/components/Header/HeaderTitle/index.tsx
@@ -1,5 +1,10 @@
 import { PropsWithChildren } from "react";
+import { Link } from "react-router-dom";
 
 export function HeaderTitle({ children }: PropsWithChildren) {
-  return <h1 className="text-2xl sm:text-4xl font-bold">{children}</h1>;
+  return (
+    <Link to="/">
+      <h1 className="text-2xl sm:text-4xl font-bold">{children}</h1>
+    </Link>
+  );
 }
