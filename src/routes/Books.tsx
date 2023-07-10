60d7bb59984a96baf400e82cf50c43acbea61440
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-10 13:24:29 +0300

    refactor(routes): update Books route
    Add padding to container
    Add display of error in alert
    Move error inside container
    Replace text with alert

--- src/routes/Books.tsx
+++ src/routes/Books.tsx
@@ -2,13 +2,15 @@ import { useEffect } from "react";
 import { useInfiniteQuery, useQueryClient } from "@tanstack/react-query";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
+import { AlertError, AlertInfo } from "../components/Alert";
 import { CardGrid } from "../components/CardGrid";
 import { Spinner } from "../components/Spinner";
 
 import { ALERT_TEXT } from "../constants/alertText";
 
 import { fetchBooks } from "../services/api/fetchBooks";
-import { Alert } from "../components/Alert";
+
+import { extractErrorMessage } from "../utils/errorMessage";
 
 export function Books() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -21,7 +23,7 @@ export function Books() {
   const currentPage = searchParams.get("page");
   const isMissingParameters = !searchQuery || !selectedCategory || !selectedSort || !currentPage;
 
-  const { data, isError, isLoading, isRefetching, fetchNextPage, hasNextPage, isFetchingNextPage, refetch } =
+  const { data, error, isError, isLoading, isRefetching, fetchNextPage, hasNextPage, isFetchingNextPage, refetch } =
     useInfiniteQuery({
       queryKey: ["books"],
       queryFn: ({ pageParam }) => fetchBooks(searchQuery, selectedCategory, selectedSort, pageParam),
@@ -45,29 +47,33 @@ export function Books() {
     };
   }, [searchQuery, selectedCategory, selectedSort]);
 
-  if (isError) return <h1 className="text-error text-bold text-xl">Something wrong happened! Try again</h1>;
-
   if (isLoading || (!isFetchingNextPage && isRefetching)) return <Spinner />;
 
   return (
     <>
-      <div className="flex flex-col gap-4 items-center w-full">
-        <h1 className="font-medium opacity-50">Found {resultsCount} results</h1>
-        {resultsCount === 0 && <Alert>{ALERT_TEXT}</Alert>}
-        {data && <CardGrid pages={data.pages} />}
-        {resultsCount !== 0 && (
-          <button
-            className="btn btn-primary"
-            disabled={!hasNextPage || isFetchingNextPage}
-            onClick={() => {
-              const updatedSearchParams = new URLSearchParams(searchParams.toString());
-              updatedSearchParams.set("page", String(Number(currentPage) + 1));
-              setSearchParams(updatedSearchParams.toString());
-              fetchNextPage();
-            }}
-          >
-            {isFetchingNextPage ? "Loading more..." : hasNextPage ? "Load More" : "Nothing more to load"}
-          </button>
+      <div className="flex flex-col gap-4 items-center w-full p-4">
+        {isError ? (
+          <AlertError>Something wrong happened! Error details: {extractErrorMessage(error)}</AlertError>
+        ) : (
+          <>
+            <h1 className="font-medium opacity-50">Found {resultsCount} results</h1>
+            {resultsCount === 0 && <AlertInfo>{ALERT_TEXT}</AlertInfo>}
+            <CardGrid pages={data.pages} />
+            {resultsCount !== 0 && (
+              <button
+                className="btn btn-primary"
+                disabled={!hasNextPage || isFetchingNextPage}
+                onClick={() => {
+                  const updatedSearchParams = new URLSearchParams(searchParams.toString());
+                  updatedSearchParams.set("page", String(Number(currentPage) + 1));
+                  setSearchParams(updatedSearchParams.toString());
+                  fetchNextPage();
+                }}
+              >
+                {isFetchingNextPage ? "Loading more..." : hasNextPage ? "Load More" : "Nothing more to load"}
+              </button>
+            )}
+          </>
         )}
       </div>
     </>
