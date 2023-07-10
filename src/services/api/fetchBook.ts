15c73776022e9a77be48c450446cf6cad13ab256
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-10 13:12:21 +0300

    feat(services): add fetchBook function

---
+++ src/services/api/fetchBook.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+import { VolumeSchema } from "../../schemas/api/volume";
+
+export const fetchBook = async (id: string | undefined) => {
+  if (!id) throw new Error("No ID provided");
+
+  const res = await axios.get(`${import.meta.env.VITE_BOOKS_API_URL}/${id}`, {
+    params: {
+      key: import.meta.env.VITE_BOOKS_API_KEY,
+    },
+  });
+  return VolumeSchema.parseAsync(res.data);
+};
