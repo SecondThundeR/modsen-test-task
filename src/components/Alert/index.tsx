fae1fa6b979336b92c3c8322266fd7b39a4b1849
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-10 13:21:43 +0300

    feat(components): update Alert component
    Add AlertError variant
    Rename Alert to AlertInfo

--- src/components/Alert/index.tsx
+++ src/components/Alert/index.tsx
@@ -1,7 +1,16 @@
 import { PropsWithChildren } from "react";
-import { InformationCircleIcon } from "@heroicons/react/24/outline";
+import { InformationCircleIcon, ExclamationCircleIcon } from "@heroicons/react/24/outline";
 
-export function Alert({ children }: PropsWithChildren) {
+export function AlertError({ children }: PropsWithChildren) {
+  return (
+    <div className="alert alert-error sm:w-3/5 lg:w-2/5 xl:w-2/6">
+      <ExclamationCircleIcon className="h-6 w-6 stroke-current shrink-0" />
+      <span>{children}</span>
+    </div>
+  );
+}
+
+export function AlertInfo({ children }: PropsWithChildren) {
   return (
     <div className="alert alert-info sm:w-3/5 lg:w-2/5 xl:w-2/6">
       <InformationCircleIcon className="h-6 w-6 stroke-current shrink-0" />
