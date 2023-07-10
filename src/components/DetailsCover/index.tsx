fc233fb37d421fad9fae0e239ccfd7c42951bccf
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-10 14:50:00 +0300

    feat(DetailsCover): add `<picture>` support
    Now picture variants resize properly

--- src/components/DetailsCover/index.tsx
+++ src/components/DetailsCover/index.tsx
@@ -3,23 +3,14 @@ import { VolumeType } from "../../schemas/api/volume";
 export const DetailsCover = (props: VolumeType["volumeInfo"]) => {
   const { title, imageLinks } = props;
 
-  if (
-    !imageLinks ||
-    !imageLinks.small ||
-    !imageLinks.smallThumbnail ||
-    !imageLinks.thumbnail ||
-    !imageLinks.medium ||
-    !imageLinks.large ||
-    !imageLinks.extraLarge
-  )
+  if (!imageLinks || !imageLinks.small || !imageLinks.medium || !imageLinks.large || !imageLinks.extraLarge)
     return <h1 className="text-xl sm:text-2xl text-center">No cover {title && `for "${title}"`}</h1>;
 
   return (
-    <img
-      className="h-[320px] sm:h-[512px] lg:h-[640px] aspect-auto"
-      srcSet={`${imageLinks.small} 1x, ${imageLinks.medium} 2x, ${imageLinks.large} 3x`}
-      src={imageLinks.large}
-      alt={`${title} cover`}
-    />
+    <picture>
+      <source srcSet={imageLinks.medium} media="(min-width: 768px) and (max-width: 1199px)" />
+      <source srcSet={imageLinks.large} media="(min-width: 1200px)" />
+      <img src={imageLinks.small} alt={`${title} cover`} />
+    </picture>
   );
 };
