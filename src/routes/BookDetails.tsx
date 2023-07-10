08e25fe43b8b20bdc5839250809033e3f1dc187c
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-10 13:47:36 +0300

    feat(routes): update BookDetails
    Add support for AlertError
    Add additional check for empty data object (in case API returns something unusual)
    Add rounding for cover div

--- src/routes/BookDetails.tsx
+++ src/routes/BookDetails.tsx
@@ -1,7 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
-import { fetchBook } from "../services/api/fetchBook";
+
+import { AlertError, AlertInfo } from "../components/Alert";
 import { Spinner } from "../components/Spinner";
+
+import { fetchBook } from "../services/api/fetchBook";
+
+import { isObjectEmpty } from "../utils/object";
 
 export function BookDetails() {
   const { id } = useParams<{ id: string }>();
@@ -12,23 +17,17 @@ export function BookDetails() {
     enabled: id !== undefined,
   });
 
+  if (isError) return <AlertError error={error} />;
+
   if (isLoading) return <Spinner />;
 
-  if (isError)
-    return (
-      <div>
-        <h1>Error!</h1>
-        <p>{JSON.stringify(error, null, 4)}</p>
-      </div>
-    );
-
-  if (!data) return <h1>No data :c</h1>;
+  if (!data || isObjectEmpty(data)) return <AlertInfo>API returned nothing :c</AlertInfo>;
 
   const { volumeInfo } = data;
 
   return (
     <div className="flex flex-col lg:flex-row w-full h-full">
-      <div className="flex justify-center items-center bg-base-200 p-6 w-full lg:w-3/6">
+      <div className="flex justify-center items-center bg-base-200 p-6 w-full lg:w-3/6 rounded-xl">
         <img
           className="h-[320px] sm:h-[512px] lg:h-[640px] aspect-auto"
           srcSet={`${volumeInfo.imageLinks?.small} 1x, ${volumeInfo.imageLinks?.medium} 2x, ${volumeInfo.imageLinks?.large} 3x`}
