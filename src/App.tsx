6805c4be42477a8d5f6b8da623904df5e233d839
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-10 14:08:29 +0300

    refactor: add eslint disable rule for QueryClient import
    For some reason, plugin thinks react-query package doesn't have such import

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/named
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
