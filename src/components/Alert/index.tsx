5b58fcd5252e3e66d4aa645dc67bd87d25532972
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-10 13:34:25 +0300

    feat(components): move error extraction inside AlertError

--- src/components/Alert/index.tsx
+++ src/components/Alert/index.tsx
@@ -1,11 +1,13 @@
 import { PropsWithChildren } from "react";
 import { InformationCircleIcon, ExclamationCircleIcon } from "@heroicons/react/24/outline";
 
-export function AlertError({ children }: PropsWithChildren) {
+import { extractErrorMessage } from "../../utils/errorMessage";
+
+export function AlertError({ error }: { error: unknown }) {
   return (
     <div className="alert alert-error sm:w-3/5 lg:w-2/5 xl:w-2/6">
       <ExclamationCircleIcon className="h-6 w-6 stroke-current shrink-0" />
-      <span>{children}</span>
+      <span>Something wrong happened! Error details: {extractErrorMessage(error)}</span>
     </div>
   );
 }
