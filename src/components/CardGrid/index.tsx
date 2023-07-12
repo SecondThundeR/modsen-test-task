f9c03294db9b79b5300337026ef02791db0d90b4
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-12 12:50:47 +0300

    feat(components): add 5th column for 2xl breakpoint

--- src/components/CardGrid/index.tsx
+++ src/components/CardGrid/index.tsx
@@ -10,7 +10,7 @@ export function CardGrid({ pages }: CardGridProps) {
   const itemsArray = pages?.flatMap(({ items }) => items);
 
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
       {itemsArray?.map(
         (item) => item && <BookCard key={item.etag} {...item} />,
       )}
