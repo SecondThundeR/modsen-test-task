df55ee25956305b66b5147d4beb853c35be24bd4
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-12 12:17:30 +0300

    fix: remove require from plugin import

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -5,7 +5,7 @@ const config = {
   semi: true,
   printWidth: 80,
   bracketSpacing: true,
-  plugins: [require("prettier-plugin-tailwindcss")],
+  plugins: ["prettier-plugin-tailwindcss"],
 };
 
 module.exports = config;
